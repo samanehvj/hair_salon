22a038fd582e75bc3c7e8df18d63d79fe1aa51d6
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-11-30 22:03:24 -0800

    Create  index.php

---
+++ index.php
@@ -0,0 +1,183 @@
+<?php
+include 'includes/functions.php';
+include 'includes/head.php';
+include 'includes/nav.php';
+?>
+
+
+	<div class="container-fluid ">
+
+		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+				<li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active ">
+					<img src="imgs/slide/slide1.jpg" class=" w-100" alt="...">
+					<div class="carousel-caption  float-xl-left ml-5 pt-5  text-center">
+						<h1 class="display-4 float-right ml-md-5 mt-md-5 d-none d-md-block" style="font-family: Inknut Antiqua;font-size:70px;">Hair Extensions</h1>
+						<h5 class="col-10  text-center  ">Schedule your appointment today.</h5>
+						<br>
+						<a href="contact.php/#booking" class="offset-md-3 mt-5  col-5 btn btn-outline-light ">Book Today</a>
+					</div>
+				</div>
+				<div class=" carousel-item">
+					<img src="imgs/slide/slide5.png" class="d-block w-100" alt="...">
+					<div class="carousel-caption  ">
+						<h1 class="display-4 float-right ml-md-5 mt-md-5 d-none d-md-block" style="font-family: Inknut Antiqua;font-size:100px;color:#320377;">Time To Change</h1>
+
+						<a href="service.php" class="offset-md-3 mt-5 col-5 btn btn-outline-light ">Learn More</a>
+
+
+					</div>
+				</div>
+
+				<div class=" carousel-item">
+					<img src="imgs/slide/slide3.png" class="d-block w-100" alt="...">
+					<div class="carousel-caption ">
+					</div>
+				</div>
+				<!-- <div class=" carousel-item">
+					<img src="imgs/slide/front.png" class="d-block w-100" alt="...">
+					<div class="carousel-caption">
+
+						<a href="contact.php/#infomation" class="col-2 btn btn-outline-light ">Call US</a>
+
+					</div>
+				</div> -->
+				<div class="carousel-item">
+					<img src="imgs/slide/slide4.png" class="d-block w-100" alt="...">
+					<div class="carousel-caption ">
+						<h1 class="display-4 float-right ml-md-5 mt-md-5 d-none d-md-block " style="font-family: Inknut Antiqua;font-size:100px;">
+							TIPS FOR
+
+							MEN</h1>
+
+						<a href="portfolio.php" class="offset-md-3 mt-5 col-5 btn btn-outline-light ">Book Today</a>
+
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+	 <div class="container">
+	<h5 class="col text-center mt-5 mb-4" style="font-size: 40px;font-family: Inknut Antiqua;">Services </h5>
+
+	<div class="row offset-lg-1">
+		<div class="col-md-4 col-lg-2 my-4">
+			<div class="col text-center">
+				<img src="imgs/service/women-style.jpg" alt="women_style_img" class="rounded-circle sv-rounded-circle">
+			</div>
+			<div class="col text-center mt-3 ml-5 ">
+				<a class="btn col img-inf-btn">Women</a>
+			</div>
+		</div>
+		<div class="col-md-4 col-lg-2 my-4">
+			<div class="col text-center">
+				<img src="imgs/service/men-style.jpg" alt="men_style_img" class="rounded-circle sv-rounded-circle">
+			</div>
+			<div class="col text-center mt-3 ml-5 ">
+				<a class="btn  col img-inf-btn">Men</a>
+			</div>
+		</div>
+		<div class="col-md-4 col-lg-2 my-4">
+			<div class="col text-center">
+				<img src="imgs/service/color.jpg" alt="coloring_style_img" class="rounded-circle sv-rounded-circle">
+			</div>
+			<div class="col text-center mt-3 ml-5 ">
+				<a class="btn col img-inf-btn">Coloring</a>
+			</div>
+		</div>
+		<div class="col-md-4 col-lg-2 my-4">
+			<div class="col text-center">
+				<img src="imgs/service/extensions.jpg" alt="extensions_style_img" class="rounded-circle sv-rounded-circle">
+			</div>
+			<div class="col text-center mt-3 ml-5">
+				<a class="btn col img-inf-btn">Extensions</a>
+			</div>
+		</div>
+		<div class="col-md-4 col-lg-2 my-4">
+			<div class="col text-center">
+				<img src="imgs/service/bride.jpg" alt="bride_style_img" class="rounded-circle sv-rounded-circle">
+			</div>
+			<div class="col text-center mt-3 ml-5">
+				<a class="btn col  img-inf-btn">Bridal</a>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="row">
+		<div class="col-md-5 youshi">
+			<div class="col ">
+				<a data-fancybox="col gallery" href="imgs/service/team6.png">
+					<img class="col float-md-right " src="imgs/service/team6.png" alt="...">
+				</a>
+			</div>
+			<div class="col ">
+				<a data-fancybox="gallery col " href="imgs/service/logo1.png">
+					<img class="col-5 float-md-right" src="imgs/service/logo1.png" alt="...">
+				</a>
+				<a data-fancybox="gallery col" href="imgs/service/logo2.png">
+					<img class="col-5 float-md-right" src="imgs/service/logo2.png" alt="...">
+				</a>
+			</div>
+
+		</div>
+		<div class="col-md-4 mt-3">
+			<img src="imgs/service/meet.png" alt="meet youshi" class="meet">
+			<p class="youshi-bio px-4 py-5 mt-0">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktoppublishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			</p>
+		</div>
+	</div>
+	<div class="row text-center mt-5 ">
+		<h5 class="col" style="font-size: 30px;font-family: Inknut Antiqua;">Testimonio</h5>
+	</div>
+
+	<div id="carouselExampleIndicators" class="carousel slide mt-4" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="imgs/service/tesm3.png" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="imgs/service/tesm.png" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+				<img src="imgs/service/tesm2.png" class="d-block w-100" alt="...">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+  </div>
+	
+
+	<?php
+	include 'includes/footer.php';
+	?>
+
+	
