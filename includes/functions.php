f3828066e5d6b9c716168f3d7107d736106e8ac1
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-12-01 00:16:56 -0800

    Create function.php

---
+++ includes/functions.php
@@ -0,0 +1,18 @@
+
+<?php
+
+function connect()
+{
+	// return mysqli_connect('localhost', 'samanehv_saman', 'Salamazizam12', 'samanehv_youshis');
+		return mysqli_connect('localhost', 'root', 'root', 'youshis');
+		
+}
+		$URL = 'http://localhost:8888/hair_salon/';
+
+
+
+?>
+
+
+
+
