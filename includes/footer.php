f3efb523f5cad87d5c02721f3cc468f0ac154028
Samaneh Vajdi <samaneh.vajdi@hotmail.com>
2020-12-01 00:16:44 -0800

    Crete footer

---
+++ includes/footer.php
@@ -0,0 +1,121 @@
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/a424e00da0.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
+
+
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark ≈" style=" background-color: #232121;color: white;">
+
+
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-5">
+
+      <!-- Grid column -->
+      <div class="col-md-4  mx-auto mb-5 mt-5">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold text-center">Hours</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p> <span class="float-left">Monday</span> <span class="float-right">Closed</span>
+          <br>
+          <br>
+
+          <span class="float-left">Tuesday </span> <span class="float-right"> 10 AM – 7 PM</span>
+          <br>
+          <br>
+
+          <span class="float-left">Wednesday </span> <span class="float-right"> 10 AM - 7 PM</span>
+          <br>
+          <br>
+
+          <span class="float-left">Thursday</span> <span class="float-right"> 10 AM - 7 PM</span>
+          <br>
+          <br>
+
+          <span class="float-left">Friday </span> <span class="float-right"> 10 AM – 7 PM</span>
+          <br>
+          <br>
+
+          <span class="float-left">Saturday </span> <span class="float-right"> 10 AM – 7 PM</span>
+          <br>
+          <br>
+
+          <span class="float-left">Sunday</span> <span class="float-right"> 10 AM – 7 PM</span></p>
+
+        
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+
+
+      <!-- Grid column -->
+      <div class="col-md-4 mx-auto mb-4 mt-5 py-5 mr-5">
+
+        <!-- Links -->
+        <h5 class="text-uppercase font-weight-bold text-center " style="color:#bcacff;font-size: 21px;">#yoshisStyle <br><br>#yoshisalon <br><br>#yoshistyle <br><br> #yoshihaircut </h5>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 80px;">
+
+        <div class="d-flex justify-content-center" style="font-size: 35px;">
+
+          <!-- Facebook -->
+          <a class="fb-ic mx-2 ">
+            <i class="fab fa-facebook-f white-text "> </i>
+          </a>
+          <!-- Twitter -->
+          <a class="tw-ic mx-2 ">
+            <i class="fab fa-twitter white-text"> </i>
+          </a>
+
+
+          <!--Instagram-->
+          <a class="ins-ic mx-2 ">
+            <i class="fab fa-instagram white-text"> </i>
+          </a>
+
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 mx-auto mb-md-0 mb-4 mt-5">
+
+        <!-- Links -->
+        <h6 class="text-uppercase font-weight-bold text-center">Contact</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <i class="fas fa-home mr-3"></i> 443 N Rodeo Dr, Beverly Hills, CA 90210
+        </p>
+
+        <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center px-4">© 2020 Copyright: Samaneh Vajdi
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
